260fb7d513437ebf9bd1e2fe1d956f1163a4c42c
Txai Mostardeiro <redtxai@gmail.com>
2022-06-10 19:04:15 +0200

    Remove not used AxiosResponse import

--- src/components/SearchPokemon.tsx
+++ src/components/SearchPokemon.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 
 import { PokemonValue } from '../models/Pokemon.model';
 
