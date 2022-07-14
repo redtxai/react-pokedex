455ceac40ff006c5de40dc7c478e4dd1ef200975
Txai Mostardeiro <redtxai@gmail.com>
2022-07-14 22:23:18 +0200

    Fix search input

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { SearchPokemonValue } from '../../models/SearchPokemon.model';
 
@@ -9,18 +9,44 @@ interface SearchProps {
   onPokemonSelected: (index: number) => void;
 }
 
-export const Search = (props: SearchProps) => {
+export const Search = ({ pokemonList, onPokemonSelected }: SearchProps) => {
+  const timerRef = useRef(null);
+
+  const [pokemonListState, setPokemonListState] = useState([]);
+  const [inputSearchText, setInputSearchText] = useState('');
+
+  useEffect(() => {
+    setPokemonListState(pokemonList)
+  }, [pokemonList])
+
+  const filterPokemonList = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current)
+    }
+
+    if (!event.currentTarget.value || !event.currentTarget.value.trim()) {
+      setPokemonListState(pokemonList)
+      return
+    }
+    setInputSearchText(event.currentTarget.value)
+
+    timerRef.current = setTimeout(() => {
+      const a = pokemonListState.filter(pokemon => pokemon.name.includes(inputSearchText.toLowerCase()))
+      setPokemonListState(a)
+    }, 1000)
+  }
+
   return (
     <section className="search-wrapper">
-      <input className="search-input" />
+      <input className="search-input" onKeyUp={filterPokemonList}/>
       {
-        props.pokemonList.length > 0 && 
+        pokemonListState.length > 0 && 
         <ul className="search-pokemon-list">
-          {props.pokemonList.map((pokemon, index) => (
+          {pokemonListState.map((pokemon, index) => (
             <li className="search-pokemon"
-            key={index}
-            onClick={() => props.onPokemonSelected(index)}>
-              {(index + 1) + ' - ' + pokemon.name}
+              key={index}
+              onClick={() => onPokemonSelected(index)}>
+                {(index + 1) + ' - ' + pokemon.name}
             </li>
           ))}
         </ul>
