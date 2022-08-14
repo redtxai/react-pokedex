92097a827db54ab89a50545ec0f15ef759cf3351
Txai Mostardeiro <redtxai@gmail.com>
2022-08-14 18:13:21 +0200

    Add event listener for the voice playing and add the blinking effect for the voice-circle

--- src/pokedex/pokemon-data/header/PokemonDataHeader.tsx
+++ src/pokedex/pokemon-data/header/PokemonDataHeader.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
+import { SearchPokemonContext } from '../../../context/SelectPokemonProvider'
 import './PokemonDataHeader.css'
 
 export const PokemonDataHeader = () => {
+  const [isVoicePlaying, setVoicePlaying] = useState(false);
+  const { pokedexVoiceService } = useContext(SearchPokemonContext)
+
+  useEffect(() => {
+    pokedexVoiceService.speech.addEventListener('start', () => {
+      setVoicePlaying(true)
+    })
+
+    pokedexVoiceService.speech.addEventListener('end', () => {
+      setVoicePlaying(false)
+    })
+  }, [pokedexVoiceService.speech])
+
   return <header className="pokedex-header">
-    <div className="voice-circle">
-      <div className="glass-effect"></div>
+    <div className="voice-circle-wrapper">
+      <div className={`voice-circle ${isVoicePlaying ? 'voice-playing' : ''}`}>
+        <div className="glass-effect"></div>
+      </div>
     </div>
     <div className="colors-circles">
       <div className="red-circle">
--- src/services/PokedexVoiceService.service.ts
+++ src/services/PokedexVoiceService.service.ts
@@ -1,19 +1,21 @@
 import { PokemonValue } from '../models/Pokemon.model';
 
 export class PokedexVoiceService {
+  speech = new SpeechSynthesisUtterance()
 
   setNewPokemonSelected(selectedPokemon: PokemonValue) {
-      this.playVoice(this.mountSelectionPokemonText(selectedPokemon))
+    this.playVoice(this.mountSelectionPokemonText(selectedPokemon))
   }
 
   private playVoice(text: string) {
-    window.speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+    this.speech.text = text
+    window.speechSynthesis.speak(this.speech);
   }
 
   private mountSelectionPokemonText(selectedPokemon: PokemonValue): string {
     const { name, types, species } = selectedPokemon
     const typesParsed = types.map(({ name }) => name)
 
-    return `${name}, ${typesParsed.toString().replace(',', '-')} Pokemon. ${species.flavorTextEntries[0].flavorText}`
+    return `${name}. ${typesParsed.toString().replace(',', '-')} Pokemon. ${species.flavorTextEntries[0].flavorText}`
   }
 }
